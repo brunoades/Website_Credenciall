ef40a6076b1d8f1fa5f6595c8299ca3abaa3833d
Bruno Alves <brunoades@gmail.com>
2021-12-07 19:58:27 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Website_Credenciall.Controllers
+{
+    public class HomeController : Controller
+    {
+        credenciall_websiteEntities _context = new credenciall_websiteEntities();
+        public ActionResult Index()
+        {
+            
+            var homeHeaderData = _context.Highlights
+                .First();
+            ViewData["HeaderTitle"] = homeHeaderData.hl_header;
+            ViewData["HeaderText"] = homeHeaderData.hl_subtitle;
+            ViewData["Slogan"] = homeHeaderData.hl_slogan;
+            ViewData["PostHeader"] = homeHeaderData.hl_post_header;
+            ViewData["PostText"] = homeHeaderData.hl_post_text;
+            ViewData["CTA"] = homeHeaderData.hl_cta;
+            return View();
+        }
+        public ActionResult AdminIndexHighlight()
+        {
+            var listOfHighlights = _context.Highlights.ToList();
+            return View(listOfHighlights);
+        }
+        [HttpGet]
+        public ActionResult AdminCreateHighlight()
+        {
+            return View();
+        }
+        public ActionResult AdminCreateHighlight(Highlight highlight)
+        {
+            _context.Highlights.Add(highlight);
+            _context.SaveChanges();
+            ViewBag.Message = "Dados salvos com sucesso!";
+            return View();
+        }
+        [HttpGet]
+        public ActionResult AdminEditHighlight(int id)
+        {
+            var data = _context.Highlights.Where(x => x.id == id).FirstOrDefault();
+            return View(data);
+        }
+        public ActionResult AdminEditHighlight(Highlight highlight)
+        {
+            var data = _context.Highlights.Where(x => x.id == highlight.id).FirstOrDefault();
+            if (data != null)
+            {
+                data.hl_header = highlight.hl_header;
+                data.hl_subtitle = highlight.hl_subtitle;
+                data.hl_post_header = highlight.hl_post_header;
+                data.hl_post_text = highlight.hl_post_text;
+                data.hl_cta = highlight.hl_cta;
+                data.active = highlight.active;
+                _context.SaveChanges();
+            }
+            return RedirectToAction("AdminIndexHighlight");
+        }
+        public ActionResult AdminDetailHighlight(int id)
+        {
+            var data = _context.Highlights.Where(x => x.id == id).FirstOrDefault();
+            return View(data);
+        }
+        public ActionResult AdminDeleteHighlight(int id)
+        {
+            var data = _context.Highlights.Where(x => x.id == id).FirstOrDefault();
+            _context.Highlights.Remove(data);
+            _context.SaveChanges();
+            ViewBag.Message = "Dado excluído com sucesso!";
+            return RedirectToAction("AdminIndexHighlight");
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
